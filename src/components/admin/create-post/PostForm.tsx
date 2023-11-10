8cbb2f830d2849490fbaefd06d0208011527152e
Uanhumen Daniel <danieluanhumen@gmail.com>
2023-11-10 20:51:05 +0100

    array error

--- src/components/admin/create-post/PostForm.tsx
+++ src/components/admin/create-post/PostForm.tsx
@@ -1,19 +1,14 @@
 import getCategories from "@/actions/getCategories";
 import Form from "./Form";
 
-type Cat = {
-  name: string;
-  id: string;
-};
-
 const PostForm = async () => {
-  const categories: Cat[] | undefined = await getCategories();
+  const categories = await getCategories();
   return (
     <div>
       <h1 className='text-center font-bold text-2xl font-lora uppercase mb-2'>
         Create new post
       </h1>
-      <Form categories={categories} />
+      {Array.isArray(categories) && <Form categories={categories} />}
     </div>
   );
 };
